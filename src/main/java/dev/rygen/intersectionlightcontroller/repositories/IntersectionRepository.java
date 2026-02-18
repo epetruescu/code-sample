d282bfd3cc9bcf60fc4bbf1f90c38f0c7662ec3a
edpetruescu <edpetruescu@gmail.com>
2026-02-17 23:03:24 -0500

    missed repo

--- src/main/java/dev/rygen/intersectionlightcontroller/repositories/IntersectionRepository.java
+++ src/main/java/dev/rygen/intersectionlightcontroller/repositories/IntersectionRepository.java
@@ -4,9 +4,14 @@ import dev.rygen.intersectionlightcontroller.entities.Intersection;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.Instant;
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface IntersectionRepository extends JpaRepository<Intersection, Integer> {
@@ -14,4 +19,11 @@ public interface IntersectionRepository extends JpaRepository<Intersection, Inte
     @Modifying
     @Query("update Intersection i set i.name = ?1, i.active = ?2 where i.intersectionId = ?3")
     void updateNameAndActiveByIntersectionIdEquals(@NonNull String name, boolean active, int intersectionId);
+
+    List<Integer> findIntersectionIdByActive();
+
+    @Transactional
+    @Modifying
+    @Query("update Intersection i set i.currentPhaseIndex = ?1, i.lastTransitionTime = ?2 where i.intersectionId = ?3")
+    int updateCurrentPhaseIndexAndLastTransitionTimeByIntersectionIdEquals(int currentPhaseIndex, Instant lastTransitionTime, int intersectionId);
 }
